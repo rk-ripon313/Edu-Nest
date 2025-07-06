763916a406a00e527c2e67ddc20f967e978a158d
rk-ripon360 <rk.ripon.rkr@gmail.com>
2025-07-06 19:54:36 +0600

    initial mongoodb set up

---
+++ database/mongooClientPromise.js
@@ -0,0 +1,28 @@
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+if (!process.env.MONGODB_URI) {
+    throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+};
+
+let client;
+
+if (process.env.NODE_ENV === "development") {
+    if (!global._mongoClient) {
+        global._mongoClient = new MongoClient(uri, options);
+    }
+    client = global._mongoClient;
+} else {
+    client = new MongoClient(uri, options);
+}
+
+module.exports = client;
